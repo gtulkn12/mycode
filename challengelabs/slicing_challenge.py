542e6292b9b018fc8b6872a0dd2e19f66949b887
Malik Stuckey <gtulkn1@yahoo.com>
2020-08-14 14:00:53 -0400

    Player Cheat Class

---
+++ challengelabs/slicing_challenge.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+easy= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+easy[2][1]
+easy[2][0]
+easy[3]
+
+print ("My" + easy[]+  ""  "! The " + "" + "do " + "" + "!")
+
+medium= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+medium[2]["googles"]
+medium[2]["eyes"]
+medium[3]
+
+hard= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+hard [0]["user"]["name"]["first"]
+hard [0]["kumquat"]
